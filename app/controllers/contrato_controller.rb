8b0b3715629bdafa0103c54176d048dd85427702
ChristianSama <christiansama1601@gmail.com>
2023-01-21 00:06:11 -0500

    feat: creación de modelos y controladores

---
+++ app/controllers/contrato_controller.rb
@@ -0,0 +1,2 @@
+class ContratoController < ApplicationController
+end
---
+++ db/migrate/20230120055407_create_contratos.rb
@@ -0,0 +1,11 @@
+class CreateContratos < ActiveRecord::Migration[7.0]
+  def change
+    create_table :contratos do |t|
+      t.decimal :monto
+      t.date :fecha
+      t.references :cliente, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
