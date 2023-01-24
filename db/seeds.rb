5b4a0b4ad5a246564a1a9880b91aaaa70c43c485
ChristianSama <christiansama1601@gmail.com>
2023-01-23 20:38:57 -0500

    feat: add db seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'date'
+
+Cliente.create([
+  {nombre: "Udla"},
+  {nombre: "Supermaxi"},
+  {nombre: "Cigarra"},
+])
+
+Contrato.create([
+  {
+    nombre: "Tour Udla",
+    monto: 4000,
+    fecha: Date.new(2023, 1, 1),
+    cliente_id: 1
+  },
+  {
+    nombre: "SEO Website",
+    monto: 2000,
+    fecha: Date.new(2023, 2, 1),
+    cliente_id: 1
+  },
+  {
+    nombre: "Nueva App Movil",
+    monto: 6000,
+    fecha: Date.new(2023, 3, 1),
+    cliente_id: 2
+  },
+  {
+    nombre: "SEO Website",
+    monto: 2500,
+    fecha: Date.new(2023, 4, 1),
+    cliente_id: 3
+  }
+])
