771730e781ab2bbb56f60d4e73dbff0c5946d144
ChristianSama <christiansama1601@gmail.com>
2023-01-25 23:09:06 -0500

    feat: agregar filtro de fechas

--- app/controllers/contratos_controller.rb
+++ app/controllers/contratos_controller.rb
@@ -2,23 +2,13 @@ class ContratosController < ApplicationController
   def index
     @contratos = Contrato.all
 
-    # solucion 1
+    fecha_inicio = params[:inicio]
+    fecha_fin = params[:fin]
+
     @totales = Contrato
       .joins(:cliente)
       .select('contratos.id, sum(contratos.monto) as monto, clientes.nombre as cliente_nombre')
       .group(:cliente_id)
-
-    # solucion 2
-    # clientes = Cliente.all
-
-    # @totales = []
-
-    # clientes.each do |cliente|
-    #   total = cliente.contratos.reduce(0) do |acc, cont|
-    #     acc + cont.monto 
-    #   end
-    #   @totales.push({:cliente_nombre => cliente.nombre, :monto => total})
-    # end
-
+      .where("fecha between (?) and (?)", fecha_inicio, fecha_fin)
   end
 end
